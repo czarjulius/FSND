5c4a36702eb452af2446e27b5e727212232b5acb
czarjulius <juliusczar.jc@gmail.com>
2019-10-19 14:35:41 +0100

    created migration for the tables

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:psql@localhost:5432/fyyur'
+DATABASE_URL = SQLALCHEMY_DATABASE_URI
